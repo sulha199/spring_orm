2c6d8515a2017da992d8bb5d9e37e0396af6c542
MITRAIS\Shulha_U3218 <sulha199@gmail.com>
2018-05-31 08:56:34 +0700

    chapter 8: Querying for a Custom Result Type with a Constructor Expression

--- src/main/java/com/apress/prospring5/ch8/SingerSummaryJPATest.java
+++ src/main/java/com/apress/prospring5/ch8/SingerSummaryJPATest.java
@@ -1,7 +1,9 @@
 package com.apress.prospring5.ch8;
 
 import com.apress.prospring5.ch8.config.JpaConfig;
+import com.apress.prospring5.ch8.service.SingerSummaryService;
 import com.apress.prospring5.ch8.service.SingerSummaryUntypeImpl;
+import com.apress.prospring5.ch8.view.SingerSummary;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,17 +20,33 @@ public class SingerSummaryJPATest {
             LoggerFactory.getLogger(SingerSummaryJPATest.class);
     private GenericApplicationContext ctx;
     private SingerSummaryUntypeImpl singerSummaryUntype;
+    private SingerSummaryService singerSummaryService;
 
     @Before
     public void setUp() {
         ctx = new AnnotationConfigApplicationContext(JpaConfig.class);
-        singerSummaryUntype = ctx.getBean(SingerSummaryUntypeImpl.class);
-        assertNotNull(singerSummaryUntype);
+//        singerSummaryUntype = ctx.getBean(SingerSummaryUntypeImpl.class);
+        //        assertNotNull(singerSummaryUntype);
+        singerSummaryService = ctx.getBean(SingerSummaryService.class);
+        assertNotNull(singerSummaryService);
     }
 
     @Test
     public void testFindAllUntype() {
         singerSummaryUntype.displayAllSingerSummary();
+    }
+
+    @Test
+    public void testFindAll() {
+        List<SingerSummary> singers = singerSummaryService.findAll();
+        listSingerSummary(singers);
+        assertEquals(2, singers.size());
+    }
+    private static void listSingerSummary(List<SingerSummary> singers) {
+        logger.info(" ---- Listing singers summary:");
+        for (SingerSummary singer : singers) {
+            System.out.println(singer.toString());
+        }
     }
 
     @After
